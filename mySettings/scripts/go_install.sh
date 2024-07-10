348f4d485664ff542df524bb2445ce8a6b1c2c30
taking <taking@duck.com>
2024-07-10 15:17:38 +0900

    [240710] go install script updated

---
+++ mySettings/scripts/go_install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Author by Taking
+# (1) go language install + env
+
+GO_URL="https://go.dev/dl"
+GO_VERSION=$(curl -s 'https://go.dev/VERSION?m=text' | head -n1)
+ARCH=$(dpkg --print-architecture)
+GO_FILE="$GO_VERSION.linux-$ARCH.tar.gz"
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+if [ -d /usr/local/go ]; then
+    echo -e "${RED}--go exist.. PASS--${NC}"
+else
+    echo -e "${RED}--go downloading...--${NC}"
+    cd ~/
+    wget --no-check-certificate --continue --show-progress $GO_URL/$GO_FILE
+    tar -xzf $GO_FILE
+    sudo mv go /usr/local
+    rm ~/$GO_FILE
+    
+    echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee -a /etc/profile
+    echo 'export GOPATH=$HOME/GO' | sudo tee -a /etc/profile
+    echo 'export PATH=$PATH:$GOPATH/bin' | sudo tee -a /etc/profile
+
+    source /etc/profile
+
+    ## Test if Golang is working
+    go version
+fi
