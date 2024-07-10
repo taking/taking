20aad554205dbb8217d1b127d205f896e6f7e699
taking <taking@duck.com>
2024-07-10 15:27:25 +0900

    [240710] java21 install script updated

---
+++ mySettings/scripts/java21_install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Author by Taking
+# (2) java language install + env
+
+ARCH=$(dpkg --print-architecture)
+DOWNLOAD_URL=https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.3%2B9/OpenJDK21U-jdk_$ARCH_linux_hotspot_21.0.3_9.tar.gz
+
+if [ -d /usr/lib/jdk-21.0.3/bin/java ]; then
+    echo -e "${RED}--java exist.. PASS--${NC}"
+else
+    echo -e "${RED}--java downloading...--${NC}"
+    cd ~/
+    wget --no-check-certificate --continue --show-progress $DOWNLOAD_URL
+    tar xvzf OpenJDK21U-jdk_x64_linux_hotspot_21.0.3_9.tar.gz
+    mv jdk-21.0.3+9/ /usr/lib/jdk-21.0.3
+    rm ~/OpenJDK21U-jdk_x64_linux_hotspot_21.0.3_9.tar.gz
+    
+    echo 'export JAVA_HOME=/usr/lib/jdk-21.0.3' | sudo tee -a /etc/profile
+    echo 'export PATH=$JAVA_HOME/bin/:$PATH' | sudo tee -a /etc/profile
+    echo 'export CLASS_PATH=$JAVA_HOME/lib:$CLASS_PATH' | sudo tee -a /etc/profile
+
+    source /etc/profile
+
+    ## Test if Java is working
+    java --version
+fi
