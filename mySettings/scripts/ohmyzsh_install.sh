4cbdcab0b3f3e1de486b38b1cbd4ffd70cf5316e
taking <taking@duck.com>
2024-07-10 16:37:37 +0900

    Fix: Minor updated

--- mySettings/scripts/ohmyzsh_install.sh
+++ mySettings/scripts/ohmyzsh_install.sh
@@ -10,7 +10,7 @@ if command -v zsh >/dev/null 2>&1; then
 else
     apt update && apt install zsh -y && chsh -s $(which zsh)
 
-    locale-gen ko_KR.UTF-8
+    locale-gen en_US.UTF-8
 
     oh-my-zsh_folder="$HOME/.oh-my-zsh/"
 
